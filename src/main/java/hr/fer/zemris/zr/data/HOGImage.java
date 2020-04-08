70b3ec5621bb088cc55c82ac2efef8f6d0f80d2c
mateoim <mateoim@me.com>
2020-04-08 13:32:59 +0200

    Add calculateHistogram method to HOGImage class

--- src/main/java/hr/fer/zemris/zr/data/HOGImage.java
+++ src/main/java/hr/fer/zemris/zr/data/HOGImage.java
@@ -3,6 +3,7 @@ package hr.fer.zemris.zr.data;
 import hr.fer.zemris.zr.util.Algorithms;
 
 import java.awt.image.BufferedImage;
+import java.util.concurrent.BlockingQueue;
 
 /**
  * A class that keeps data about histogram of gradients of the given image.
@@ -11,6 +12,36 @@ import java.awt.image.BufferedImage;
  */
 
 public class HOGImage {
+
+    /**
+     * Required width of image to be used with the algorithm.
+     */
+    private static final int DEFAULT_WIDTH = 64;
+
+    /**
+     * Required height of image to be used with the algorithm.
+     */
+    private static final int DEFAULT_HEIGHT = 128;
+
+    /**
+     * Size of the window used.
+     */
+    private static final int WINDOW_SIZE = 8;
+
+    /**
+     * Number of bins used in histograms.
+     */
+    private static final int BIN_SIZE = 9;
+
+    /**
+     * Width of a bin in angles.
+     */
+    private static final int BIN_WIDTH = 20;
+
+    /**
+     * Keeps the calculated histogram data.
+     */
+    private float[][] histogram;
 
     /**
      * Keeps the calculated magnitude of the image.
@@ -70,5 +101,96 @@ public class HOGImage {
 
         magnitude = Algorithms.calculateMagnitude(gx, gy);
         angle = Algorithms.calculateAngle(gx, gy);
+    }
+
+    /**
+     * Calculates the histogram data.
+     *
+     * @return histogram data {@link #histogram}.
+     *
+     * @throws UnsupportedOperationException if the image is not {@code 64 x 128}.
+     */
+    public float[][] calculateHistogram() {
+        if (histogram != null) {
+            return histogram;
+        }
+
+        if (width != DEFAULT_WIDTH && height != DEFAULT_HEIGHT) {
+            throw new UnsupportedOperationException("Width must be " + DEFAULT_WIDTH + " and height must be "
+                    + DEFAULT_HEIGHT + "!");
+        }
+
+        final int totalSize = DEFAULT_HEIGHT / WINDOW_SIZE * DEFAULT_WIDTH / WINDOW_SIZE;
+        histogram = new float[totalSize][BIN_SIZE];
+
+        final int processors = Runtime.getRuntime().availableProcessors();
+
+        BlockingQueue<Integer> queue = Algorithms.initializeQueue(totalSize, processors, -1);
+
+        Thread[] threads = new Thread[processors];
+
+        for (int i = 0; i < processors; i++) {
+            Thread thread = new Thread(() -> calculationThread(queue));
+            threads[i] = thread;
+
+            thread.start();
+        }
+
+        Algorithms.joinThreads(threads);
+
+        return histogram;
+    }
+
+    /**
+     * Thread job used to calculate histogram data.
+     *
+     * @param queue containing the indexes of histogram positions.
+     */
+    private void calculationThread(BlockingQueue<Integer> queue) {
+        while (true) {
+            int index;
+
+            try {
+                index = queue.take();
+            } catch (InterruptedException exc) {
+                continue;
+            }
+
+            if (index == -1) {
+                break;
+            }
+
+            calculateHistogram(index);
+        }
+    }
+
+    /**
+     * Calculates the histogram at the given index.
+     *
+     * @param position index of the histogram to be calculated.
+     */
+    private void calculateHistogram(int position) {
+        final int widthSize = DEFAULT_WIDTH / WINDOW_SIZE;
+        final int rowPosition = position / widthSize;
+        final int rowOffset = (position - (WINDOW_SIZE * rowPosition)) * WINDOW_SIZE * bytesPerPixel;
+        final int initialOffset = rowPosition * DEFAULT_WIDTH * WINDOW_SIZE * bytesPerPixel + rowOffset;
+
+        float[] histogram = this.histogram[position];
+
+        for (int i = 0; i < WINDOW_SIZE; i++) {
+            int offset = initialOffset + i * DEFAULT_WIDTH * bytesPerPixel;
+            for (int j = 0; j < WINDOW_SIZE * bytesPerPixel; j++) {
+                final float angle = this.angle[offset];
+                final float magnitude = this.magnitude[offset];
+
+                final int index = (int) (angle / BIN_WIDTH);
+                final float factor = (angle - (BIN_WIDTH * index)) / BIN_WIDTH;
+
+                histogram[index] = (1 - factor) * magnitude;
+                histogram[(index + 1) % BIN_SIZE] = factor * magnitude;
+
+                offset++;
+            }
+        }
     }
 }
